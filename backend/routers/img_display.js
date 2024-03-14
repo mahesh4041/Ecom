cc8a5675d40eaa878b605a8c9a97ef1858daee11
Mahesh Babu <maheshbabu.jasti0001@gmail.com>
2024-03-14 13:25:48 -0500

    add

--- backend/routers/img_display.js
+++ backend/routers/img_display.js
@@ -2,6 +2,7 @@ const express=require('express');
 const database = require('./database');
 const router=express.Router();
 
+
 router.get('/:id', (req, res) => {
     const { id } = req.params
     const sql = "select * from products where id=?";
